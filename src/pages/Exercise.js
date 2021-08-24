33ba40c1fc244cb51c889fd149733dc06e583584
andrewNEW42 <sanjino56@gmail.com>
2021-08-24 20:54:21 +0300

    added input field and submit button (without functionality)

--- src/pages/Exercise.js
+++ src/pages/Exercise.js
@@ -1,11 +1,38 @@
 import React from 'react';
-import { StyleSheet, Text, View , Button, TouchableOpacity} from 'react-native';
+import { StyleSheet, Text, View , Button, TouchableOpacity, TextInput} from 'react-native';
 
 export const ExerciseTemplate = ({route}) => {
     return (
     <View>
         <Text style={{fontSize:28}}>{route.params.exerciseDesc}</Text>
+        <View>
         <Text style={{fontSize:32}}>{route.params.problem}</Text>
+            <TextInput
+                style={styles.inputField}/>
+        </View>
+        <TouchableOpacity style={styles.SubmitButton}>
+            <Text>Submit</Text>
+        </TouchableOpacity>
     </View>)
 
 }
+
+const styles = StyleSheet.create({
+    inputField:{
+        width: 319,
+        height: 40, 
+        borderColor: 'gray', 
+        borderWidth: 1 
+    },
+    SubmitButton:{
+        width: 319,
+        padding: 20,
+        justifyContent:"center",
+        alignItems:"center",
+        backgroundColor:"white",
+        borderColor:"black",
+        borderRadius:50,
+        borderWidth: 3,
+        elevation:5,
+    }
+})
